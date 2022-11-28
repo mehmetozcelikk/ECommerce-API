5730968b3380570d213ff3b66136b97414807397
Mehmet Özçelik <mehmett.ozcelik@yahoo.com>
2022-11-28 10:26:23 +0300

    update product

--- Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
+++ Presentation/ECommerceAPI.API/Controllers/ProductsController.cs
@@ -1,86 +1,119 @@
-﻿using ECommerce.Application.RequestParameters;
+﻿using ECommerce.Application.Consts;
+using ECommerce.Application.CustomAttributes;
+using ECommerce.Application.Enums;
+using ECommerce.Application.Features.Commands.ProductImageFile.ChangeShowcaseImage;
+using ECommerce.Application.Features.Commands.ProductImageFile.UploadProductImage;
+using ECommerce.Application.Features.Queries.Product.GetAllProduct;
+using ECommerce.Application.Features.Queries.Product.GetByIdProduct;
+using ECommerce.Application.Features.Queries.ProductImageFile.GetProductImages;
+using ECommerce.Application.RequestParameters;
 using ECommerce.Domain.Entities;
+using ECommerceAPI.Application.Features.Commands.Product.CreateProduct;
+using ECommerceAPI.Application.Features.Commands.Product.RemoveProduct;
+using ECommerceAPI.Application.Features.Commands.Product.UpdateProduct;
 using ECommerceAPI.Application.Repositories;
 using ECommerceAPI.Application.ViewModels.Products;
+using ETicaretAPI.Application.Features.Commands.ProductImageFile.RemoveProductImage;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
-namespace ECommerceAPI.API.Controllers
+namespace ECommerceAPI.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ProductsController : ControllerBase
+    readonly IMediator _mediator;
+    readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(IMediator mediator, ILogger<ProductsController> logger)
     {
-        readonly private IProductWriteRepository _productWriteRepository;
-        readonly private IProductReadRepository _productReadRepository;
-
-        public ProductsController(
-            IProductWriteRepository productWriteRepository,
-            IProductReadRepository productReadRepository)
-        {
-            _productWriteRepository = productWriteRepository;
-            _productReadRepository = productReadRepository;
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Get([FromQuery] Pagination pagination)
-        {
-            var totalCount = _productReadRepository.GetAll(false).Count();
-            var products = _productReadRepository.GetAll(false).Skip(pagination.Page * pagination.Size).Take(pagination.Size).Select(p => new
-            {
-                p.Id,
-                p.Name,
-                p.Stock,
-                p.Price,
-                p.CreatedDate,
-                p.UpdatedDate
-            }).ToList();
-
-            return Ok(new
-            {
-                totalCount,
-                products
-            });
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id)
-        {
-            return Ok(await _productReadRepository.GetByIdAsync(id, false));
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Post(VM_Create_Product model)
-        {
-
-            await _productWriteRepository.AddAsync(new()
-            {
-                Name = model.Name,
-                Price = model.Price,
-                Stock = model.Stock
-            });
-            await _productWriteRepository.SaveAsync();
-            return StatusCode((int)HttpStatusCode.Created);
-        }
-
-        [HttpPut]
-        public async Task<IActionResult> Put(VM_Update_Product model)
-        {
-            Product product = await _productReadRepository.GetByIdAsync(model.Id);
-            product.Stock = model.Stock;
-            product.Name = model.Name;
-            product.Price = model.Price;
-            await _productWriteRepository.SaveAsync();
-            return Ok();
-        }
-
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(string id)
-        {
-            await _productWriteRepository.RemoveAsync(id);
-            await _productWriteRepository.SaveAsync();
-            return Ok();
-        }
-
+        _mediator = mediator;
+        _logger = logger;
     }
+
+    [HttpGet]
+    public async Task<IActionResult> Get([FromQuery] GetAllProductQueryRequest getAllProductQueryRequest)
+    {
+        GetAllProductQueryResponse response = await _mediator.Send(getAllProductQueryRequest);
+        return Ok(response);
+    }
+
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> Get([FromRoute] GetByIdProductQueryRequest getByIdProductQueryRequest)
+    {
+        GetByIdProductQueryResponse response = await _mediator.Send(getByIdProductQueryRequest);
+        return Ok(response);
+    }
+
+    [HttpPost]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Writing, Definition = "Create Product")]
+    public async Task<IActionResult> Post(CreateProductCommandRequest createProductCommandRequest)
+    {
+        CreateProductCommandResponse response = await _mediator.Send(createProductCommandRequest);
+        return StatusCode((int)HttpStatusCode.Created);
+    }
+
+    [HttpPut]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Updating, Definition = "Update Product")]
+    public async Task<IActionResult> Put([FromBody] UpdateProductCommandRequest updateProductCommandRequest)
+    {
+        UpdateProductCommandResponse response = await _mediator.Send(updateProductCommandRequest);
+        return Ok();
+    }
+
+    [HttpDelete("{Id}")]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Deleting, Definition = "Delete Product")]
+    public async Task<IActionResult> Delete([FromRoute] RemoveProductCommandRequest removeProductCommandRequest)
+    {
+        RemoveProductCommandResponse response = await _mediator.Send(removeProductCommandRequest);
+        return Ok();
+    }
+
+    [HttpPost("[action]")]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Writing, Definition = "Upload Product File")]
+    public async Task<IActionResult> Upload([FromQuery] UploadProductImageCommandRequest uploadProductImageCommandRequest)
+    {
+        uploadProductImageCommandRequest.Files = Request.Form.Files;
+        UploadProductImageCommandResponse response = await _mediator.Send(uploadProductImageCommandRequest);
+        return Ok();
+    }
+
+    [HttpGet("[action]/{id}")]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Reading, Definition = "Get Products Images")]
+    public async Task<IActionResult> GetProductImages([FromRoute] GetProductImagesQueryRequest getProductImagesQueryRequest)
+    {
+        List<GetProductImagesQueryResponse> response = await _mediator.Send(getProductImagesQueryRequest);
+        return Ok(response);
+    }
+
+    [HttpDelete("[action]/{id}")]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Deleting, Definition = "Delete Product Image")]
+    public async Task<IActionResult> DeleteProductImage([FromRoute] RemoveProductImageCommandRequest removeProductImageCommandRequest, [FromQuery] string imageId)
+    {
+        //Ders sonrası not !
+        //Burada RemoveProductImageCommandRequest sınıfı içerisindeki ImageId property'sini de 'FromQuery' attribute'u ile işaretleyebilirdik!
+
+        removeProductImageCommandRequest.ImageId = imageId;
+        RemoveProductImageCommandResponse response = await _mediator.Send(removeProductImageCommandRequest);
+        return Ok();
+    }
+
+    [HttpGet("[action]")]
+    [Authorize(AuthenticationSchemes = "Admin")]
+    [AuthorizeDefinition(Menu = AuthorizeDefinitionConstants.Products, ActionType = ActionType.Updating, Definition = "Change Showcase Image")]
+    public async Task<IActionResult> ChangeShowcaseImage([FromQuery] ChangeShowcaseImageCommandRequest changeShowcaseImageCommandRequest)
+    {
+        ChangeShowcaseImageCommandResponse response = await _mediator.Send(changeShowcaseImageCommandRequest);
+        return Ok(response);
+    }
+
 }
